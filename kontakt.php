2c45eaa991262a3a12cd63da404261b59d556f6c
MariiaMartyshko66766 <masamartysko@gmail.com>
2026-04-19 21:36:43 +0200

    Create kontakt.php

---
+++ kontakt.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Kontakt - 66766</title>
+</head>
+<body>
+    <header><h1>Kontakt</h1></header>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><a href="index.php">Strona Główna</a></li>
+                <li><a href="baza.php">Baza Danych</a></li>
+                <li><a href="kontakt.php">Kontakt</a></li>
+            </ul>
+        </nav>
+        <main>
+            <h2>Dane kontaktowe</h2>
+            <p>Imię i Nazwisko: Mariia Martyshko</p>
+            <p>Numer albumu: 66766</p>
+            <p>Email: m.martyshko@student.pl</p>
+        </main>
+    </div>
+    <footer><p>Mariia Martyshko 66766</p></footer>
+</body>
+</html>
