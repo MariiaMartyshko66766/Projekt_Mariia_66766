e14607faba7d2df06f75dbacba75ba2cc0a70114
MariiaMartyshko66766 <masamartysko@gmail.com>
2026-05-06 23:18:27 +0200

    Refactor contact details into a card format
    Updated contact information layout with card styling.

--- kontakt.php
+++ kontakt.php
@@ -10,17 +10,18 @@
     <header><h1>Kontakt</h1></header>
     <div class="container">
         <nav>
-            <ul>
-                <li><a href="index.php">Strona Główna</a></li>
-                <li><a href="baza.php">Baza Danych</a></li>
-                <li><a href="kontakt.php">Kontakt</a></li>
-            </ul>
+            <a href="index.php">Start</a>
+            <a href="baza.php">Baza</a>
+            <a href="kontakt.php">Kontakt</a>
         </nav>
         <main>
-            <h2>Dane kontaktowe</h2>
-            <p>Imię i Nazwisko: Mariia Martyshko</p>
-            <p>Numer albumu: 66766</p>
-            <p>Email: m.martyshko@student.pl</p>
+            <div class="card">
+                <h2>Dane kontaktowe</h2>
+                <p><b>Imię:</b> Mariia</p>
+                <p><b>Nazwisko:</b> Martyshko</p>
+                <p><b>Numer albumu:</b> 66766</p>
+                <p><b>E-mail:</b> m.martyshko@student.pl</p>
+            </div>
         </main>
     </div>
     <footer><p>Mariia Martyshko 66766</p></footer>
